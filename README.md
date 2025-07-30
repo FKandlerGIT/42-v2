10f446fa96eb08392412803d22276930574175f5
Felix Kandler <felix.kandler@icloud.com>
2025-07-30 20:18:16 +0200

    changes

--- README.md
+++
@@ -1,3 +0,0 @@
-# Libft-v2
-
-create a multisplit from ft_split; helpfull in minishell;
---
+++ minitalk/ft_minitalk.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_minitalk.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fekandle <fekandle@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/30 19:58:57 by fekandle          #+#    #+#             */
+/*   Updated: 2025/07/30 19:59:32 by fekandle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_MINITALK_H
+# define FT_MINITALK_H
+
+# include "Libft-v2/libft.h"
+# include "PrintF-v2/ft_printf.h"
+# include "ft_client.c"
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+#endif
